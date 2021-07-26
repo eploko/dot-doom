ee42eec72ce82ce915776f7ac65b2d7ec09e9d5f
Andrey Subbotin <andrey.subbotin@accelo.com>
2021-07-26 18:10:12 +1000

    Complete on digit keys

--- config.el
+++ config.el
@@ -158,6 +158,25 @@
     "Margin function which picks the appropriate icon set automatically."
     (company-text-icons-margin candidate selected)))
 
+(defun ora-company-number ()
+  "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+  (interactive)
+  (let* ((k (this-command-keys))
+         (re (concat "^" company-prefix k)))
+    (if (cl-find-if (lambda (s) (string-match re s))
+                    company-candidates)
+        (self-insert-command 1)
+      (company-complete-number (string-to-number k)))))
+
+(let ((map company-active-map))
+  (mapc
+   (lambda (x)
+     (define-key map (format "%d" x) 'ora-company-number))
+   (number-sequence 0 9)))
+
 (use-package! ledger-mode)
 
 ;; Better flycheck errors
